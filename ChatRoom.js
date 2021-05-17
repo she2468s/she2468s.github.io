dabab12ab7a3a5f848481ebcf36541b9310c4035
Angyoyo <angyoyo@Anyoyode-MacBook-Pro.local>
2021-05-17 20:36:24 +0800

    add firebase

--- ChatRoom.js
+++ ChatRoom.js
@@ -30,13 +30,21 @@ function send(messageType) {
     $("#messageDisplayArea").scrollTop($("#messageDisplayArea")[0].scrollHeight);
 }
 
-// function send2() {
-//     let message = $('#message').val();
-//     let messageWrapper2 = '<div style="display: flex; justify-content: flex-start; margin: 20px; color: white;">'
-//         + '<div style="background-color: darkgrey; max-width: 60%; border-radius: 8px; padding: 20px;">'
-//         + message
-//         + '</div>'
-//         + '</div>';
-//     $('#messageDisplayArea').append(messageWrapper2);
-//     $("#messageDisplayArea").scrollTop($("#messageDisplayArea")[0].scrollHeight);
-// }
+const messaging = firebase.messaging();
+messaging.usePublicVapidKey('BJK5AGnOwtOILLi13QssRk3x0K1s0GKj-Jd5-8u3YlHtTAft9cw2XQexVtBOo3cTnH7ThG6yzlHovQC_yH7-SQY');
+messaging.requestPermission()
+    .then(res => {
+        // 若允許通知 -> 向 firebase 拿 token
+        return messaging.getToken();
+    }, err => {
+        // 若拒絕通知
+        console.log(err);
+    })
+    .then(token => {
+        // 成功取得 token
+        console.log(token);
+    })
+
+messaging.onMessage(payload => {
+    console.log(payload);
+});
---
+++ firebase-messaging-sw.js
@@ -0,0 +1,34 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.5.0/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+    apiKey: "AIzaSyDZ88lZGpin2oTYGqO8YtTPqGZIcxWOgpA",
+    authDomain: "chatroom-4bbe7.firebaseapp.com",
+    projectId: "chatroom-4bbe7",
+    storageBucket: "chatroom-4bbe7.appspot.com",
+    messagingSenderId: "507339669544",
+    appId: "1:507339669544:web:be474138d40ee4e83c751d",
+    measurementId: "G-GRPWQ12LGT"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.'
+    };
+
+    self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
